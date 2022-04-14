da96c4deefeaf9081aece7b843d57fde497baa7b
Adithyan M.S <adithyan2ms@gmail.com>
2022-04-14 12:47:07 +0530

    break at 1.36.00 hours

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const dropZone=document.querySelector(".drop-zone")
 const browseBtn=document.querySelector(".browseBtn")
 const fileInput=document.querySelector("#fileInput")
 
+const progressContainer=document.querySelector(".progress-container")
+const bgProgress=document.querySelector(".bg-progress")
+const progressBar=document.querySelector(".progress-bar")
+
+const percentDiv=document.querySelector("#percent")
+
 const host = "https://innshare.herokuapp.com/";
-const uploadURL= `${host}api/files`;
-//const uploadURL= `${host}api/files`;
+const uploadURL = `${host}api/files`
+
+// const uploadURL = `${host}api/files`
 
 
 dropZone.addEventListener("dragover",(e)=>{
@@ -20,7 +27,7 @@ dropZone.addEventListener("dragleave",()=>{
 dropZone.addEventListener("drop",(e)=>{
     e.preventDefault()
     dropZone.classList.remove("dragged");
-    const files=e.dataTransfer.files
+    const files=e.dataTransfer.files;
     console.log(files);
     if(files.length){
         fileInput.files=files;
@@ -28,21 +35,42 @@ dropZone.addEventListener("drop",(e)=>{
     }
 })
 
+fileInput.addEventListener("change",()=>{
+    uploadFile()
+})
+
 browseBtn.addEventListener("click",()=>{
     fileInput.click()
 })
 
 const uploadFile=()=>{
-    const file=fileInput.files[0]
+    progressContainer.style.display="block"
+    const file=fileInput.files[0];
     const formData=new FormData()
     formData.append("myfile",file)
-
     const xhr=new XMLHttpRequest();
+
     xhr.onreadystatechange=()=>{
-        if(xhr.readyState===XMLHttpRequest.DONE){
+        if (xhr.readyState === XMLHttpRequest.DONE) {
             console.log(xhr.response);
+            showLink(JSON.parse(xhr.response))
         }
-    };
-    xhr.open("POST",uploadURL);
+    }
+    xhr.upload.onprogress=updateProgress;
+    xhr.open("POST",uploadURL)
     xhr.send(formData)
+
+}
+
+const updateProgress=(e)=>{
+    const percent=Math.round((e.loaded / e.total) * 100);
+    // console.log(percent);
+    bgProgress.style.width = '${percent}%';
+    percentDiv.innerText=percent;
+    progressBar.style.transform='scaleX(${percent/100})'
+}
+const showLink=({file})=>{
+    console.log(file);
+    progressContainer.style.display="none"
+    
 }
