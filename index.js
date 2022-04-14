a30f15f2dbf2d6f534cc9456391f340e07e21b95
Adithyan M.S <adithyan2ms@gmail.com>
2022-04-14 23:46:49 +0530

    frontend completed

--- index.js
+++ index.js
@@ -8,10 +8,21 @@ const progressBar=document.querySelector(".progress-bar")
 
 const percentDiv=document.querySelector("#percent")
 
+const sharingContainer=document.querySelector(".sharing-container")
+const fileURLInput=document.querySelector("#fileURL")
+const copyBtn=document.querySelector("#copyBtn")
+
+const emailForm=document.querySelector("#emailForm")
+
+const toast=document.querySelector(".toast")
+
+
 const host = "https://innshare.herokuapp.com/";
 const uploadURL = `${host}api/files`
 
-// const uploadURL = `${host}api/files`
+const emailURL = `${host}api/files/send`
+
+const maxAllowedSize=100 * 1024 *1024; //100mb
 
 
 dropZone.addEventListener("dragover",(e)=>{
@@ -43,9 +54,27 @@ browseBtn.addEventListener("click",()=>{
     fileInput.click()
 })
 
+copyBtn.addEventListener("click",()=>{
+    fileURLInput.select()
+    document.execCommand("copy")
+    showToast("Link Copied")
+})
+
 const uploadFile=()=>{
-    progressContainer.style.display="block"
+    if (fileInput.files.length>1) {
+        resetFileInput()
+        showToast("Only upload 1 file")
+        return;
+    }
     const file=fileInput.files[0];
+
+    if (file.size > maxAllowedSize ) {
+        showToast("Can't upload more than 100MB");
+        resetFileInput();
+        return;
+    }
+    progressContainer.style.display="block"
+
     const formData=new FormData()
     formData.append("myfile",file)
     const xhr=new XMLHttpRequest();
@@ -53,10 +82,15 @@ const uploadFile=()=>{
     xhr.onreadystatechange=()=>{
         if (xhr.readyState === XMLHttpRequest.DONE) {
             console.log(xhr.response);
-            showLink(JSON.parse(xhr.response))
+            onUploadSuccess(JSON.parse(xhr.response))
         }
     }
     xhr.upload.onprogress=updateProgress;
+
+    xhr.upload.onerror=()=>{
+    resetFileInput()
+        showToast('Error in upload: ${xhr.statusText}')
+    }
     xhr.open("POST",uploadURL)
     xhr.send(formData)
 
@@ -69,8 +103,52 @@ const updateProgress=(e)=>{
     percentDiv.innerText=percent;
     progressBar.style.transform='scaleX(${percent/100})'
 }
-const showLink=({file})=>{
-    console.log(file);
+const onUploadSuccess=({file : url})=>{
+    console.log(url);
+resetFileInput()
+    emailForm[2].removeAttribute("disabled");
     progressContainer.style.display="none"
-    
+    sharingContainer.style.display="block"
+    fileURLInput.value=url;
+}
+
+const resetFileInput = ()=>{
+    fileInput.value="";
+}
+
+emailForm.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    console.log("submit form");
+    const url =fileURLInput.value
+    const formData={
+        uuid:url.split("/").splice(-1,1)[0],
+        emailTo:emailForm.elements("to-email").value,
+        emailFrom:emailForm.elements("from-email").value
+    }
+    console.log(formData);
+
+    emailForm[2].setAttribute("disabled","true")
+    fetch(emailURL,{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(formData)
+    })
+    .then(res => res.json())
+    .then((success) => {
+        if (success) {
+            sharingContainer.style.display="none"
+            showToast("Email Sent")
+        }
+    })
+})
+let toastTimer;
+const showToast=(msg)=>{
+    toast.innerText=msg;
+    toast.style.transform="translate(-50%,0)"
+    clearTimeout(toastTimer)
+    toastTimer = setTimeout(()=>{
+        toast.style.transform="translate(-50%,60px)"
+    },2000)
 }
